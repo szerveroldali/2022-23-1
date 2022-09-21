2bed89073d1a1791705953a6832e524ee1c39011
Tűri Erik <54734700+turierik@users.noreply.github.com>
2022-09-21 12:33:46 +0200

    blog templates

---
+++ turi_erik/nappali/laravel_blog_feladat/sablonok/main.html
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Blog - {{ $title }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{ $slot }}
+        </div>
+    </body>
+</html>
---
+++ turi_erik/nappali/laravel_blog_feladat/sablonok/post-data.blade.php
@@ -0,0 +1,25 @@
+@php
+    $num = 3;
+    $posts = [
+        (object)[
+            'title'   => 'Egy almafa',
+            'author'  => 'John Doe',
+            'content' => 'Aliquam erat volutpat. Maecenas erat turpis, malesuada sed lorem quis, tincidunt bibendum orci. Cras non ipsum a nibh posuere tristique.'
+        ],
+        (object)[
+            'title'   => 'Két katica',
+            'author'  => 'Yamada Hanako',
+            'content' => 'Etiam cursus lectus vel accumsan aliquam. Maecenas dapibus blandit felis vel ultricies. Proin vitae ante id sem interdum sagittis.'
+        ],
+        (object)[
+            'title'   => 'Három kiscica',
+            'author'  => 'Petar Petrović',
+            'content' => 'Mauris finibus id sem a pulvinar. In metus ante, consequat vel neque eget, ultrices eleifend lacus. Duis blandit elementum imperdiet.'
+        ],
+        (object)[
+            'title'   => 'Négy porszívó',
+            'author'  => 'Kim Yuna',
+            'content' => 'Nunc vehicula congue ipsum sed tincidunt. Aenean ut nibh leo. Aenean ut justo quis risus maximus consequat sed ac lorem.'
+        ],
+    ];
+@endphp
