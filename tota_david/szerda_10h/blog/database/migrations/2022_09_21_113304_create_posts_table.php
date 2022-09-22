057241d904d3a4c2454d0a43e492616124333b25
Tóta Dávid <totadavid95@gmail.com>
2022-09-22 13:05:58 +0200

    [Wed10] Practice 2

---
+++ tota_david/szerda_10h/blog/database/migrations/2022_09_21_113304_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('text');
+            $table->string('description')->nullable();
+            $table->string('cover_image_path')->nullable();
+            $table->boolean('hidden')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
