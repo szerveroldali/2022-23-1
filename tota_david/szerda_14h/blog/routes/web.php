945e80849dfbb6c1e1ed49b0e82864956660f910
Tóta Dávid <totadavid95@gmail.com>
2022-10-05 15:35:36 +0200

    [Wed14] Practice 4

--- tota_david/szerda_14h/blog/routes/web.php
+++ tota_david/szerda_14h/blog/routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts/create', function () {
+    return view('posts.create');
+});
+
+Route::get('/posts/x', function () {
+    return view('posts.show');
+});
+
+Route::get('/posts/x/edit', function () {
+    return view('posts.edit');
+});
+
+// -----------------------------------------
+
+Route::get('/categories/create', function () {
+    return view('categories.create');
+});
+
+Route::get('/categories/x', function () {
+    return view('categories.show');
+});
+
+// -----------------------------------------
+
+Auth::routes();
