586f63966859f87faeb2ea158c46dca800a189f3
Tóta Dávid <totadavid95@gmail.com>
2022-10-12 15:39:16 +0200

    [Wed14] Practice 5

--- tota_david/szerda_14h/blog/routes/web.php
+++ tota_david/szerda_14h/blog/routes/web.php
@@ -1,48 +1,58 @@
 <?php
 
+// use App\Models\User;
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::route('posts.index');
 });
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+// Route::resource('posts', PostController::class);
+// Route::resource('categories', CategoryController::class);
 
-Route::get('/posts/create', function () {
-    return view('posts.create');
-});
-
-Route::get('/posts/x', function () {
-    return view('posts.show');
-});
-
-Route::get('/posts/x/edit', function () {
-    return view('posts.edit');
-});
-
-// -----------------------------------------
-
-Route::get('/categories/create', function () {
-    return view('categories.create');
-});
-
-Route::get('/categories/x', function () {
-    return view('categories.show');
-});
-
-// -----------------------------------------
+// Egységesítve (ugyanaz, mint fent):
+Route::resources([
+    'posts' => PostController::class,
+    'categories' => CategoryController::class
+]);
 
 Auth::routes();
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/posts', function () {
+//     return view('posts.index', [
+//         'users_count' => User::count(),
+//     ]);
+// })->name('posts.index');
+
+// Route::get('/posts/create', function () {
+//     return view('posts.create');
+// });
+
+// Route::get('/posts/x', function () {
+//     return view('posts.show');
+// });
+
+// Route::get('/posts/x/edit', function () {
+//     return view('posts.edit');
+// });
+
+// // -----------------------------------------
+
+// Route::get('/categories/create', function () {
+//     return view('categories.create');
+// });
+
+// Route::get('/categories/x', function () {
+//     return view('categories.show');
+// });
+
+// -----------------------------------------
+
